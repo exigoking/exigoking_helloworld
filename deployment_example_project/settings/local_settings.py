69e87ffc6c2be86563da88f69aedcc9dd32af0b9
Timur <user@users-MacBook-Pro.local>
2015-04-20 13:52:38 +0600

    6th commit

--- deployment_example_project/settings/local_settings.py
+++ deployment_example_project/settings/local_settings.py
@@ -1,14 +1,1 @@
-DEBUG = False
 
-DATABASES = {
-    'default': {
-        'NAME': 'exigoking_database_0001',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
-        'PASSWORD': 'Iphone4S',
-        'HOST': 'ec2-52-28-50-208.eu-central-1.compute.amazonaws.com',
-        'PORT': '',
-    },
-}
-
-ALLOWED_HOSTS = ["exigokinghelloworld", "ec2-52-28-54-11.eu-central-1.compute.amazonaws.com"]
